ac924886fb54e41d358466c361e9d04af31e99eb
prashdevlop <lolariyaprashant@gmail.com>
2019-02-10 15:35:48 +0530

    added security group

--- Terraform/Simple-1/ec2.tf
+++ Terraform/Simple-1/ec2.tf
@@ -1,6 +1,12 @@
 resource "aws_instance" "example" {
   ami = "ami-0ad42f4f66f6c1cc9"
   instance_type = "t2.micro"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
   tags {
     Name = "terraform-example"
   }
