c22c83bcf94a54b8c783b8eb349a9c8c73af5e65
prashdevlop <lolariyaprashant@gmail.com>
2019-02-12 01:49:12 +0530

    sg test

--- Simple-1/ec2.tf
+++ Simple-1/ec2.tf
@@ -1,15 +1,27 @@
 resource "aws_instance" "example" {
-  ami = "ami-0ad42f4f66f6c1cc9"
+  ami = "${var.my_ami}"
   instance_type = "t2.micro"
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install httpd -y
-              service httpd start
-              chkconfig httpd on
-              EOF
-  security_groups = ["terraform-example-instance"]
+
+  #key_name = "${var.key_name}"
+
+  #count=3
+
+  #Assign a pre-created security group
+  security_groups = ["DMZ"]
+
+  # We run a remote provisioner on the instance after creating it.
+  # In this case, we just install nginx and start it. By default,
+  # this should be on port 80
+  user_data = "${file("userdata.sh")}"
+  #user_data = <<-EOF
+  #            #!/bin/bash
+  #            yum update -y
+  #            yum install httpd -y
+  #            service httpd start
+  #            chkconfig httpd on
+  #            EOF
+  #security_groups = ["terraform-example-instance"]
   tags {
-    Name = "terraform-example"
+    Name = "${var.tag_name}"
   }
 }
