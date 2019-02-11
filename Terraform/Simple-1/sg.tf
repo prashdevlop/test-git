2476a36f66960debe2e9c7973d7ffc049dcaf598
prashdevlop <lolariyaprashant@gmail.com>
2019-02-12 00:42:47 +0530

    changes

--- Terraform/Simple-1/sg.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-  ingress {
-    from_port = 0
-    to_port = 0
-    protocol = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
