cee87b619503d07e457dc2f57f9ba2b3ab7b3719
prashdevlop <lolariyaprashant@gmail.com>
2019-02-12 00:40:48 +0530

    0 in sg

--- Simple-1/sg.tf
+++ Simple-1/sg.tf
@@ -1,8 +1,8 @@
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
   ingress {
-    from_port = 80
-    to_port = 80
+    from_port = 0
+    to_port = 0
     protocol = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
