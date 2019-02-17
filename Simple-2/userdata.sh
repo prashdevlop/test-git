ee3817345740a5bab324a0fe484868965db111c7
prashdevlop <lolariyaprashant@gmail.com>
2019-02-17 17:08:05 +0530

    new

--- Simple-2/userdata.sh
+++ Simple-2/userdata.sh
@@ -3,6 +3,7 @@ sudo yum update –y
 sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+sudo yum install java -y
 sudo yum install jenkins -y
 sudo service jenkins start
 cd
