f7c59503fd95936fd818269ce023b4de8626a1a7
prashdevlop <lolariyaprashant@gmail.com>
2019-02-21 00:19:43 +0530

    adding docker variables

--- S-1/build.sh
+++ S-1/build.sh
@@ -3,6 +3,10 @@ sudo yum update -y
 sudo yum install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
-cat ~/password.txt | docker login --username prashdevlop --password-stdin
-sudo docker build -t prashdevlop/newtest:v1 .
-sudo docker push prashdevlop/newtest:v1
+DOCKER_USER=`cat ~/user.txt`
+DOCKER_PASS=`cat ~/password.txt`
+#For docker v-18
+#cat ~/password.txt | docker login --username prashdevlop --password-stdin
+docker login --username=$DOCKER_USER --password=$DOCKER_PASS
+sudo docker build -t $DOCKER_USER/newtest:v1 .
+sudo docker push $DOCKER_USER/newtest:v1
