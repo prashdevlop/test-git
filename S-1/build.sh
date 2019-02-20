a33966c7ca0c9fe857f2301655b15891ea45015e
prashdevlop <lolariyaprashant@gmail.com>
2019-02-21 00:04:46 +0530

    adding docker automation

--- S-1/build.sh
+++ S-1/build.sh
@@ -4,4 +4,4 @@ sudo yum install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
 cat ~/password.txt | docker login --username prashdevlop --password-stdin
-sudo docker build -t prashdevlop/newtest:v1
+sudo docker build -t prashdevlop/newtest:v1 .
