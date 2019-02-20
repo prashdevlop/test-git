7f4ac5a5a35895afc1c4a17fb67bb206e32a8a4f
prashdevlop <lolariyaprashant@gmail.com>
2019-02-21 00:40:38 +0530

    new changes

--- Simple-2/userdata.sh
+++ Simple-2/userdata.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
-sudo yum update –y
-sleep 20
-sudo yum install wget -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
-sudo yum install java -y
-sleep 30
-sudo yum install jenkins -y
-sudo service jenkins start
+yum update –y
+yum install wget -y
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+yum install java -y
+yum install jenkins -y
+service jenkins start
 cd
 mkdir .ssh
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC15KHtQUkNQMdQb4FuyH5nR8Dyjj85p7bJ95ljqL6u6L3O1PyBZloEEmQnundNM2PKilvk8piH5Tqs1VmNFctZ9MrreIv+hgE0nvKacLXjO5SxiLcJm2MAXhYx58SaXM9dSJXHyrOPdYCMgVaIBmYOCkuJGXWTrEXeJDa1EII4lSkVwsa6axjOAMAAkLAj5CAXo1gUliXW9rpe/zn4brucunJApgFXwwIS7LFmrR2iOrWRbKagOWqeM97fzSwxwqsgebl7Jf0mxwMFPG61l3zaBAySHbFd9CBTE4NYiD2myjpIQ/ZerrJx4Qa6P3bG38F/E1TPbOhWek1+8RSoR0Ph Prashant@DESKTOP-E4P0MD8" >> .ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDI47R+izsAsYd2sYhdvFKmtIg8pKRW1K5nfQXho8AuN/MbV4J5lS7E/7wHQTI4QYCvknXATRnT+iQWeOQIA2NRQh+hNFNZ5gekksu8IBcjoJKS/hLnSA25xMFf5Z5YE+/xaGfwh9vzfhMSIJwm/RCagxOVrW/kEyFc4/ZkIQUEvwmtoFU7OpdjDF8iAenBHDz6DDgeu2a3qO0spVsHuOG7OjvFnUW8Ku9uwYL9rpOQLzr/T1syX/uyY3qGtOQfUMW/qiaf4++Ix4v65gYBwMs9PszTkUXlda7GDiVqcWIHQnwII/UJm2npALblNC/WWy4JXbCjqTJLH9X+YobedrZF Prashant@DESKTOP-E4P0MD8" >> .ssh/authorized_keys
 chmod 600 .ssh/authorized_keys
