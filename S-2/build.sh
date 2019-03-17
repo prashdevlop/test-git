62a84a092a8e17417b0ce886a036ae131b8b1c68
prashdevlop <lolariyaprashant@gmail.com>
2019-03-17 23:03:04 +0530

    test

--- S-2/build.sh
+++ S-2/build.sh
@@ -3,8 +3,8 @@ sudo yum update -y
 sudo yum install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
-DOCKER_USER=`cat ~/user.txt`
-DOCKER_PASS=`cat ~/password.txt`
+DOCKER_USER=`cat /root/user.txt`
+DOCKER_PASS=`cat /root/password.txt`
 #For docker v-18
 #cat ~/password.txt | docker login --username prashdevlop --password-stdin
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
