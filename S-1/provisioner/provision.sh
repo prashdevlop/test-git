5d833996246041e9a759cb8e456fdbaac511f736
prashdevlop <lolariyaprashant@gmail.com>
2019-03-18 23:30:44 +0530

    Adding Packer with config file

--- S-1/provisioner/provision.sh
+++ S-1/provisioner/provision.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install -y epel-release
-sudo yum install -y nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
