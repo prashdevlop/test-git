fa300ee41da5084ad62ac2a5c42777137087e612
Prashant Lolariya <32552776+prashdevlop@users.noreply.github.com>
2020-07-22 12:30:45 +0530

    Update ec2.tf

--- Simple-1/ec2.tf
+++ Simple-1/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "example" {
-  ami = "${var.my_ami}"
+  ami = var.my_ami
   instance_type = "t2.micro"
 
-  #key_name = "${var.key_name}"
+  #key_name = var.key_name
 
   #count=3
 
@@ -12,16 +12,5 @@ resource "aws_instance" "example" {
   # We run a remote provisioner on the instance after creating it.
   # In this case, we just install nginx and start it. By default,
   # this should be on port 80
-  user_data = "${file("userdata.sh")}"
-  #user_data = <<-EOF
-  #            #!/bin/bash
-  #            yum update -y
-  #            yum install httpd -y
-  #            service httpd start
-  #            chkconfig httpd on
-  #            EOF
-  #security_groups = ["terraform-example-instance"]
-  tags {
-    Name = "${var.tag_name}"
-  }
+  user_data = file("userdata.sh")
 }
